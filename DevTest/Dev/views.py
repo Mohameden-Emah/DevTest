6c2d8756e1c5c579e8fdb9480b04f83263a68f10
Mohameden-Emah <bedenahmedou@gmail.com>
2021-12-20 23:16:19 +0100

    je fait l'ajout du consultation

--- DevTest/Dev/views.py
+++ DevTest/Dev/views.py
@@ -7,16 +7,32 @@ def home(request):
     return render(request, 'home.html', {'Consult' : Consult})
 
 def AjoutConsultation(request):
-    # nom = request.POST['nom']
-    # prenom = request.POST['prenom']
-    # Date_Naissance = request.POST['Date_Naissance']
-    # sex = request.POST['sex']
-    # groupe_Sanguin = request.POST['groupe_Sanguin']
-    # Date_Consultation = request.POST['Date_Consultation']
-    # poids = request.POST['poids']
-    # Tension = request.POST['Tension']
-    # Taille = request.POST['Taille']
-    # Observation = request.POST['Observation']
+    Consult = Consultation.objects.all()
+    if request.method == 'POST' :
+        nom = request.POST['nom']
+        prenom = request.POST['prenom']
+        Date_Naissance = request.POST['Date_Naissance']
+        sex = request.POST['sex']
+        groupe_Sanguin = request.POST['groupe_Sanguin']
+        Date_Consultation = request.POST['Date_Consultation']
+        Poids = request.POST['Poids']
+        Tension = request.POST['Tension']
+        Taille = request.POST['Taille']
+        Observation = request.POST['Observation']
+        
+        consultation = Consultation.objects.create(
+            nom=nom,
+            prenom=prenom,
+            Date_Naissance=Date_Naissance,
+            sex=sex,
+            groupe_Sanguin=groupe_Sanguin,
+            Date_Consultation=Date_Consultation,
+            Poids=Poids,
+            Tension=Tension,
+            Taille=Taille,
+            Observation=Observation
+        )
 
-    return render(request, 'AjoutConsultation.html')
+    return render(request, 'AjoutConsultation.html', {'Consult ' : Consult})
+
    
